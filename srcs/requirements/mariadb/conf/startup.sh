183e5825cda8cbf458c569db2f3c949d9548aa33
mpoussie <mpoussie@inception.myguest.virtualbox.org>
2024-12-17 14:52:38 +0100

    add dockerfile mariadb and startup.sh

--- srcs/requirements/mariadb/conf/startup.sh
+++ srcs/requirements/mariadb/conf/startup.sh
@@ -1,3 +1,11 @@
 # !/bin/sh
 
-mariadb-uroot -e "CREATE DATABASE IF NOT EXISTS ${DB};"
+# Crée la base de données, et vérifie si elle existe avant.
+mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS ${DB};"
+
+# Crée un user, et vérifie s'il existe avant et y ajoute un mot de passe.
+mariadb -uroot -e "CREATE USER IF NOT EXISTS ${DB_USER} '@localhost' IDENTIFIED BY ${DB_USER_PSW};"
+
+# Donne tous les privilèges aux users crée précédemment.
+mariadb -uroot -e "GRANT ALL PRIVILEGES ON ${DB} TO ${DB_USER} '@'%' IDENTIFIED BY ${DB_USER_PSW};"
+
