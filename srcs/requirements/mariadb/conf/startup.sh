2f2ff73b4de0d0b82c57e68ebff836554f2dd1a1
Mehdi <47466257+MXK7@users.noreply.github.com>
2024-12-17 17:32:11 +0100

    add waiting cmd startup.sh mariadb

--- srcs/requirements/mariadb/conf/startup.sh
+++ srcs/requirements/mariadb/conf/startup.sh
@@ -1,5 +1,11 @@
 # !/bin/sh
 
+# -uroot : Se connecte en tant que root.
+# -e : Exécute la commande suivante.
+# ${DB} : Nom de la base de données.
+# ${DB_USER} : Nom de l'utilisateur.
+# ${DB_USER_PSW} : Mot de passe de l'utilisateur.
+
 # Crée la base de données, et vérifie si elle existe avant.
 mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS ${DB};"
 
@@ -9,5 +15,5 @@ mariadb -uroot -e "CREATE USER IF NOT EXISTS ${DB_USER} '@localhost' IDENTIFIED 
 # Donne tous les privilèges aux users crée précédemment.
 mariadb -uroot -e "GRANT ALL PRIVILEGES ON ${DB} TO ${DB_USER} '@'%' IDENTIFIED BY ${DB_USER_PSW};"
 
-# 
-mariadb-admin -uroot -p$MYSQL_ROOT_PASSWORD --wait-for-all-slaves shutdown
+# Fait patienter les enfants en attendant les parents. 
+mariadb-admin -uroot -p $MYSQL_ROOT_PASSWORD --wait-for-all-slaves shutdown
